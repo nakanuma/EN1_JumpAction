a2f3f9c8fc6ca7e06ea8775bedd5b55496735ae2
nakanuma <k023g0121@g.neec.ac.jp>
2024-06-10 11:54:14 +0900

    作業内容をプッシュ

---
+++ Assets/ItemScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    private Animator animator;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Enter");
+        // ê⁄êGÇµÇΩÇÁè¡ñ≈
+        //DestroySelf();
+        animator.SetTrigger("Get");
+        audioSource.Play();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log("Stay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("Exit");
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/PullingJump.cs
+++ Assets/PullingJump.cs
@@ -12,6 +12,7 @@ public class PullingJump : MonoBehaviour
     }
 
     private Vector3 clickPosition;
+    private bool isCanJump;
     [SerializeField]
     private float jumpPower = 10;
     // Update is called once per frame
@@ -21,7 +22,7 @@ public class PullingJump : MonoBehaviour
         {
             clickPosition = Input.mousePosition;
         }
-        if (Input.GetMouseButtonUp(0))
+        if (isCanJump && Input.GetMouseButtonUp(0))
         {
             // クリックした座標と離した座標の差分を取得
             Vector3 dist = clickPosition - Input.mousePosition;
@@ -32,5 +33,36 @@ public class PullingJump : MonoBehaviour
             // 重力係数を設定
             Physics.gravity = new Vector3(0, -9.8f, 0);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("衝突した");
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        //Debug.Log("接触中");
+
+        // 衝突している点の情報が複数格納されている
+        ContactPoint[] contacts = collision.contacts;
+        // 0番目の衝突情報から、衝突している点の法線を取得
+        Vector3 otherNormal = contacts[0].normal;
+        // 上方向を示すベクトル。長さは1。
+        Vector3 upVector = new Vector3(0, 1, 0);
+        // 上方向と法線の内積
+        float dotUN = Vector3.Dot(upVector, otherNormal);
+        // 内積値に逆三角関数arccosを掛けて角度を算出。それを弧度法へ変換
+        float dotDeg = Mathf.Acos(dotUN) * Mathf.Rad2Deg;
+        // 二つのベクトルがなす角度が45度より小さければ再びジャンプ可能とする。
+        if(dotDeg <= 45)
+        {
+            isCanJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //Debug.Log("離脱した");
+        isCanJump = false;
     }
 }
